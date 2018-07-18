481e6955b2ce77a330ba378c6550210f73ffe0da
Derek R <yalnilami@gmail.com>
2018-07-18 14:16:14 -0400

    Module 9: Linting

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import './index.css';
 
 import numeral from 'numeral';
 
+//since this was just a test file, I'll disable linting
+/* eslint-disable */
+
 const courseValue = numeral(1000).format('$0,0.00');
 debugger;
 console.log(`I would pay ${courseValue} for this awesome course!`);
